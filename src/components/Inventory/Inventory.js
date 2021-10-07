12872f261e6f212640caf14582399019b6f1778d
Mahmudul Hasan Numan <mhnuman95@gmail.com>
2021-10-07 22:48:24 +0600

    update inventory page

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Inventory = () => {
     return (
         <div>
-            <h1>This is Inventory</h1>
+            <h1>This is Inventory page</h1>
+            <p>Its time to knoow about the inventory</p>
         </div>
     );
 };
